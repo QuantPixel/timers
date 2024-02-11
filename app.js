7816e7f5135f3769df9358e2d655986afaee8331
QuantPixel <49346078+QuantPixel@users.noreply.github.com>
2024-02-11 13:54:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-window.addEventListener('load', () => {
-
-    if ('serviceWorker' in navigator){
-
-        navigator.serviceWorker.register('sw.js')
-            .then(registration => {
-                console.log('Service worker successfully registered', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed', error);
-            });
-    }
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(function(registration) {
+      console.log(
+        'Service Worker registration successful with scope: ',
+        registration.scope
+      );
+    })
+    .catch(function(err) {
+      console.log('Service Worker registration failed: ', err);
+    });
+}
